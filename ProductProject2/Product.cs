6c6584e3b9a1dbb3a09cc68306bc7f282d9a86f4
Ali Sobhani <acdota@outlook.com>
2023-07-06 16:33:28 +0330

    First version 1.01

---
+++ ProductProject2/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProductProject2
+{
+    public class Product
+    {
+        private const int MIN_PRODUCTNAME_LENGTH = 3;
+        private const string EMAIL_CORRECT_PATTERN = @"/[a-zA-Z0-9.-]+@[a-z]{2-3}/";
+        private const string MOBILE_PHONE_CORRECT_PATTERN = @"{4}[0-9]?-{3}[0-9]?-([1-9]2|[1-9";
+        private const string HOME_PHONE_CORRECT_PATTERN = @"";
+
+
+        public string Name { get; private set; }
+        public DateTime ProduceDate { get; private set; }
+        public bool IsAvailable { get; private set; }
+        public string ManufactureEmail { get; private set; }
+        public string ManufacturePhone { get; private set; }
+
+
+        public Product(string name, DateTime produceDate, bool isAvailable, string mfEmail, string mfPhone)
+        {
+            
+
+            Name = name;
+            ProduceDate = produceDate;
+            IsAvailable = isAvailable;
+            ManufactureEmail = mfEmail;
+            ManufacturePhone = mfPhone;
+
+        }
+
+        #region Validation
+        
+
+        #endregion
+    }
+}
